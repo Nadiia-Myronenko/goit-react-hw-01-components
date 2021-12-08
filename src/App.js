73b8190be82bb92efaa6d1010b98ee28f9a75fd8
Nadiia-Myronenko <nadegda.burina@gmail.com>
2021-12-08 18:36:58 +0200

    made component Profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 //import "./App.css";
-
 import React from "react";
+import Painting from "./components/social-profile/Profile";
+import user from "./components/social-profile/user.json";
 
 const App = () => {
-  return <div>Lorem ipsum sit amet</div>;
+  return (
+    <Painting
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
 };
 
 export default App;
