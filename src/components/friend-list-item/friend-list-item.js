bfcfc330136c76b9ef912cbd93e67541646d0fe0
Nadiia-Myronenko <nadegda.burina@gmail.com>
2021-12-14 10:26:38 +0200

    made better friend list item component

--- src/components/friend-list-item/friend-list-item.js
+++ src/components/friend-list-item/friend-list-item.js
@@ -3,14 +3,10 @@ import PropTypes from "prop-types";
 import s from "./Friend-list-item.module.css";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
+  const statusType = isOnline ? "StatusOnline" : "StatusOffline";
   return (
     <li className={s.Item}>
-      {isOnline ? (
-        <span className={s.Status + " " + s.StatusOnline}></span>
-      ) : (
-        <span className={s.Status}></span>
-      )}
-
+      <span className={`${s.Status} ${s[statusType]}`}></span>
       <img className={s.Avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.Name}>{name}</p>
     </li>
